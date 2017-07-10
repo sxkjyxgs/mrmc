224f73efb7ce9cbf5a9331e63cf77b9f450bf6fa
nichenhao <1634121368@qq.com>
2017-07-10 15:31:30 +0800

    删除

--- PC/src/page/goods/add-goods.vue
+++
@@ -1,8 +0,0 @@
-<!--<template>-->
-  <!--<div id="box">-->
-    <!--<div class="box">-->
-      <!--<div class="add_goods_title mid">添加商品</div>-->
-    <!--</div>-->
-  <!--</div>-->
-<!--</template>-->
-
